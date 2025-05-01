2ffe74bc2a4ea9bdf523a25a85bf7b56963db8df
Robin OBrien <robin.c.obrien@gmail.com>
2025-05-01 11:20:47 -0400

    Initial Commit

---
+++ DailyStack/DailyStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyStackApp.swift
+//  DailyStack
+//
+//  Created by Robin O'Brien on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct DailyStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
